4d92417a1f00dce053f3012a0e02379c1d306d02
Aleksey Ivanovskiy <alexei.ivanovski@gmail.com>
2018-10-26 18:33:01 +0300

    Start implementing DrawerLayout.

--- app/src/main/kotlin/com/ivanovsky/passnotes/presentation/unlock/UnlockActivity.kt
+++ app/src/main/kotlin/com/ivanovsky/passnotes/presentation/unlock/UnlockActivity.kt
@@ -3,6 +3,8 @@ package com.ivanovsky.passnotes.presentation.unlock
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.view.MenuItem
 
 import com.ivanovsky.passnotes.R
 import com.ivanovsky.passnotes.presentation.core.BaseActivity
@@ -16,13 +18,19 @@ class UnlockActivity : BaseActivity() {
 		}
 	}
 
+	lateinit var drawer: DrawerLayout
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
-		setContentView(R.layout.core_base_activity)
+		setContentView(R.layout.core_base_activity_with_side_menu)
 
 		setSupportActionBar(findViewById(R.id.tool_bar))
 		currentActionBar.title = getString(R.string.app_name)
+		currentActionBar.setDisplayHomeAsUpEnabled(true)
+		currentActionBar.setHomeAsUpIndicator(R.drawable.ic_add_white_24dp)
+
+		drawer = findViewById(R.id.drawer_layout)
 
 		val fragment = UnlockFragment.newInstance()
 		supportFragmentManager.beginTransaction()
@@ -31,5 +39,15 @@ class UnlockActivity : BaseActivity() {
 
 		val presenter = UnlockPresenter(this, fragment)
 		fragment.setPresenter(presenter)
+	}
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
+		return when (item.itemId) {
+			android.R.id.home -> {
+				drawer.showContextMenu()
+				true
+			}
+			else -> super.onOptionsItemSelected(item)
+		}
 	}
 }

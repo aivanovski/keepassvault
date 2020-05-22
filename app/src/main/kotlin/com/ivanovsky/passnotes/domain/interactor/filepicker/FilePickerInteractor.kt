d32c40c75ad098b29a456c4c3e9a6e24d676c4dd
Aleksey Ivanovsky <alexei.ivanovski@gmai.com>
2020-05-22 17:17:53 +0300

    Clean up build file

--- app/src/main/kotlin/com/ivanovsky/passnotes/domain/interactor/filepicker/FilePickerInteractor.kt
+++ app/src/main/kotlin/com/ivanovsky/passnotes/domain/interactor/filepicker/FilePickerInteractor.kt
@@ -3,9 +3,6 @@ package com.ivanovsky.passnotes.domain.interactor.filepicker
 import com.ivanovsky.passnotes.data.entity.FileDescriptor
 import com.ivanovsky.passnotes.data.entity.OperationResult
 import com.ivanovsky.passnotes.data.repository.file.FileSystemResolver
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 
 class FilePickerInteractor(private val fileSystemResolver: FileSystemResolver) {
 

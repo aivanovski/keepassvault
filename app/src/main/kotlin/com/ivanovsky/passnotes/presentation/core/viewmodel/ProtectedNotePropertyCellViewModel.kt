25d0dd64b4d6fd5bbd344e3522ead8ae6cd6fba1
Aleksey Ivanovsky <alexei.ivanovski@gmail.com>
2022-03-05 19:31:19 +0100

    Fix multiline properties in Note screen

--- app/src/main/kotlin/com/ivanovsky/passnotes/presentation/core/viewmodel/ProtectedNotePropertyCellViewModel.kt
+++ app/src/main/kotlin/com/ivanovsky/passnotes/presentation/core/viewmodel/ProtectedNotePropertyCellViewModel.kt
@@ -1,20 +1,27 @@
 package com.ivanovsky.passnotes.presentation.core.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import com.ivanovsky.passnotes.presentation.core.BaseCellViewModel
 import com.ivanovsky.passnotes.presentation.core.event.Event.Companion.toEvent
 import com.ivanovsky.passnotes.presentation.core.event.EventProvider
-import com.ivanovsky.passnotes.presentation.core.model.NotePropertyCellModel
+import com.ivanovsky.passnotes.presentation.core.model.ProtectedNotePropertyCellModel
 
-class NotePropertyCellViewModel(
-    override val model: NotePropertyCellModel,
+class ProtectedNotePropertyCellViewModel(
+    override val model: ProtectedNotePropertyCellModel,
     private val eventProvider: EventProvider
 ) : BaseCellViewModel(model) {
+
+    val isValueHidden = MutableLiveData(true)
+
+    fun onVisibilityButtonClicked() {
+        isValueHidden.value = !(isValueHidden.value ?: false)
+    }
 
     fun onCopyButtonClicked() {
         eventProvider.send((COPY_BUTTON_CLICK_EVENT to model.value).toEvent())
     }
 
     companion object {
-        val COPY_BUTTON_CLICK_EVENT = NotePropertyCellViewModel::class.qualifiedName + "_copyTextClickEvent"
+        val COPY_BUTTON_CLICK_EVENT = ProtectedNotePropertyCellViewModel::class.qualifiedName + "_copyTextClickEvent"
     }
 }

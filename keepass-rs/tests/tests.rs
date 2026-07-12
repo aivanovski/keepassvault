2ee92b9932f899e3386182b6af5e8958fc212394
Aliaksei Ivanouski <alexei.ivanovski@gmail.com>
2026-07-12 12:02:07 +0200

    Move test data out of tests.rs

--- keepass-rs/tests/tests.rs
+++ keepass-rs/tests/tests.rs
@@ -1,5 +1,8 @@
+pub mod test_data;
+
 #[cfg(test)]
 mod tests {
+    use crate::test_data::{TestDatabaseKey, TEST_DATABASES};
     use keepass_rs::decode::decode;
     use keepass_rs::encode::encode;
     use keepass_rs::error::BridgeError;
@@ -7,39 +10,6 @@ mod tests {
     use std::fs;
     use std::io::Error;
     use std::path::PathBuf;
-
-    struct Key {
-        password: Option<&'static str>,
-        key_content: Option<&'static str>,
-    }
-    struct TestDatbase {
-        path: &'static str,
-        key: Key,
-    }
-
-    const TEST_DATABASES: &[TestDatbase] = &[
-        TestDatbase {
-            path: "tests/resources/test-password.kdbx",
-            key: Key {
-                password: Some("abc123"),
-                key_content: None,
-            },
-        },
-        TestDatbase {
-            path: "tests/resources/test-key-file.kdbx",
-            key: Key {
-                password: None,
-                key_content: Some("def456"),
-            },
-        },
-        TestDatbase {
-            path: "tests/resources/test-password-key-file.kdbx",
-            key: Key {
-                password: Some("abc123"),
-                key_content: Some("def456"),
-            },
-        },
-    ];
 
     #[test]
     fn should_decode_database() {
@@ -56,7 +26,7 @@ mod tests {
     }
 
     #[test]
-    fn should_return_error_when_key_is_invali() {
+    fn should_return_error_when_key_is_invalid() {
         // arrange
         let test_database = TEST_DATABASES.first().unwrap();
         let db_bytes = read_file(test_database.path).unwrap();
@@ -85,7 +55,7 @@ mod tests {
             let db_bytes = read_file(test_database.path).unwrap();
             let key = convert_key(&test_database.key);
             let database = decode(&db_bytes, key.clone()).unwrap();
-            let expected = get_expected(&database);
+            let expected = get_expected_database(&database);
 
             // act
             let encoded = encode(database, key.clone());
@@ -94,7 +64,6 @@ mod tests {
             let actual = decode(&encoded.unwrap(), key.clone()).unwrap();
             assert_eq!(actual.config, expected.config);
             assert_eq!(actual.meta, expected.meta);
-            // TODO: uncomment
             assert_eq!(actual.root_group, expected.root_group);
             assert_eq!(actual.attachments, expected.attachments);
             assert_eq!(actual.custom_icons, expected.custom_icons);
@@ -102,7 +71,7 @@ mod tests {
         }
     }
 
-    fn get_expected(source: &proto::Database) -> proto::Database {
+    fn get_expected_database(source: &proto::Database) -> proto::Database {
         proto::Database {
             config: source.config.clone().map(|config| proto::DatabaseConfig {
                 version: Some(proto::DatabaseVersion {
@@ -129,7 +98,7 @@ mod tests {
         fs::read(path)
     }
 
-    fn convert_key(key: &Key) -> proto::DatabaseKey {
+    fn convert_key(key: &TestDatabaseKey) -> proto::DatabaseKey {
         proto::DatabaseKey {
             password: key.password.map(|p| p.to_string()),
             key_bytes: key.key_content.map(|k| k.to_string().into_bytes()),

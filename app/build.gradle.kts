1bf8cec83ba1fab697a2c61870f24d1a930c9286
Aliaksei Ivanouski <alexei.ivanovski@gmail.com>
2026-08-05 22:36:39 +0200

    Update version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ plugins {
 }
 
 val versionMajor = 2
-val versionMinor = 0
-val versionPatch = 2
+val versionMinor = 1
+val versionPatch = 0
 
 val formattedVersionCode = (versionMajor * 10000 + versionMinor * 100 + versionPatch).toString()
 val formattedVersionName = "$versionMajor.$versionMinor.$versionPatch"
